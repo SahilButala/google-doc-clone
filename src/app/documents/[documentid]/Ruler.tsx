e36197a8514e9bf57d8af8e59140f79fa155e748
sahil <sahil06butala@gmail.com>
2025-07-05 17:32:18 +0530

    adding ruler file

---
+++ src/app/documents/[documentid]/Ruler.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Ruler = () => {
+  return (
+    <div className='h-6 border-b border-gray-300 flex items-center relative print:hidden select-none'>
+        <div id='ruler-container'  className='max-w-[816px]'> 
+
+        </div>
+    </div>
+  )
+}
+
+export default Ruler
--- src/app/documents/[documentid]/editor.tsx
+++ src/app/documents/[documentid]/editor.tsx
@@ -22,6 +22,7 @@ import { FontSizeExtension } from "../../../extensions/font-size";
 
 import { Color } from "@tiptap/extension-color";
 import { LineHeightExtension } from "@/extensions/line-height";
+import Ruler from "./Ruler";
 
 const Editor = () => {
   const { setEditor } = useEditorStore();
@@ -110,6 +111,7 @@ const Editor = () => {
 
   return (
     <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0   print:bg-white print:overflow-visible">
+      <Ruler/>
       <div className="min-w-max justify-center w-[816px] py-4  print:py-0 mx-auto print:w-full print:min-w-0 flex">
         <EditorContent editor={editor} />
       </div>
